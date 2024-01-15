4b3b2ddc307442fab764522c1f454b391802c615
Omar Sh. Dieh <omarsheikhdieh@gmail.com>
2024-01-15 09:50:39 +0100

    update solution with dic

--- src/books.js
+++ src/books.js
@@ -1,5 +1,41 @@
 // Iteration 1 | Create Books Array
 
+// const string = "abcasdwertghlgijasdkljcdqweascasqsdasdc";
+// const dic = {};
+// string.split("").forEach((char, i) => {
+//   if (!dic[char]) {
+//     dic[char] = 0;
+//   }
+//   dic[char] += 1;
+//   dic[e] = (dic[e] || 0) + 1;
+// });
+// console.log(dic);
+
+// const dictionary2 = {
+//   "J. K. Rowling": [
+//     ["Harry Potter and the Philosopher's Stone", 223],
+//     ["Harry Potter and the Chamber of Secrets", 251],
+//     ["Harry Potter and the Prisoner of Azkaban", 317],
+//     ["Harry Potter and the Goblet of Fire", 636],
+//   ],
+//   "Neal Stephenson": [
+//     ["Cryptonomicon", 928],
+//     ["Anathem", 1008],
+//     ["Fall; or, Dodge in Hell", 896],
+//   ],
+//   "Malcolm Gladwell": [
+//     ["Outliers", 320],
+//     ["Blink", 287],
+//   ],
+// };
+
+// for (const key in dictionary2) {
+//   // console.log(dictionary2[key]);
+//   dictionary2[key].forEach((element, index) => {
+//     console.log(key, element);
+//   });
+// }
+
 const booksArray = [
   {
     title: "The Old Man and the Sea",
@@ -11,7 +47,6 @@ const booksArray = [
         "One of Hemingway's most famous works, it tells the story of Santiago...",
     },
   },
-
   {
     title: "The Airbnb Story",
     pages: 256,
@@ -53,28 +88,24 @@ const booksArray = [
   },
 ];
 
-
+// The Art of Learning - Josh Waitzkin - 288 pages
 
 // Iteration 2 | Book Details
 function getBookDetails(book) {
   return `${book.title} - ${book.author} - ${book.pages} pages`;
 }
 
-
-
 // Iteration 3 | Delete Lanuguage
 booksArray.forEach((book) => {
   delete book.details.language;
 });
 
-
-
 // Iteration 4 | Estimated Reading Time
 booksArray.forEach((book) => {
   book.readingTime = Math.ceil((book.pages * 500) / 90);
 });
-
-
+// Math.ceil => 1.01 => 2
+// Math.floor => 1.99 => 1
 
 // Bonus: Iteration 5 | Books Dictionary
 
@@ -99,9 +130,9 @@ const dictionary = {
   ],
 };
 
-function booksByAuthor(dictionary){
+function booksByAuthor(dictionary) {
   // Your code here:
-  let result = []
+  let result = [];
 
   for (author in dictionary) {
     const authorBooks = dictionary[author];
@@ -113,23 +144,21 @@ function booksByAuthor(dictionary){
       const newBook = {
         title: title,
         pages: pages,
-        author: author
+        author: author,
       };
 
       result.push(newBook);
-    })
-  };
+    });
+  }
 
   return result;
 }
-
 
 // Bonus: Iteration 6 | Average Reading Time
 function averagePageCount(books) {
   let totalPages = 0;
 
-  console.log('books', books)
-
+  // console.log('books', books)
 
   for (let i = 0; i < books.length; i++) {
     totalPages += books[i].pages;
